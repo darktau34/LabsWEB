9f818176c791ea53fae66044cec0d131e6ae4417
darktau34 <100190836+darktau34@users.noreply.github.com>
2022-11-03 11:54:10 +0300

    Add files via upload

---
+++ LR3/php/login.php
@@ -0,0 +1,239 @@
+<?php
+require_once 'process_login.php';
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="../css/bootstrap.css">
+    <link rel="stylesheet" href="../css/style.css">
+    <script src="../js/bootstrap.js"></script>
+    <script src="../js/bootstrap.bundle.js"></script>
+    <title>Сеть доставки пиццы</title>
+</head>
+<body style="background-color: #e7e7e7">
+<div class="container-fluid">
+    <div class="row" style="background-color: #ff8c00">
+        <div class="col-md-1">
+            <div class="p-1"> </div>
+        </div>
+        <div class="col-md-2">
+            <div class="p-1" style="color: white">
+                <a href="#" class="text-decoration-none" style="color: white">
+                    <img src="../img/icons/location.png" height="20" width="20"></img>
+                    Волгоград
+                </a>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="p-1">
+                <a href="#" style="padding-right: 20px; color: white" class="text-decoration-none">Магазины</a>
+                <a href="#" style="padding-right: 20px; color: white" class="text-decoration-none">Покупателям</a>
+                <a href="#" style="padding-right: 20px; color: white" class="text-decoration-none">Акции</a>
+                <a href="#" style="padding-right: 20px; color: white" class="text-decoration-none">Клубная карта</a>
+            </div>
+        </div>
+        <div class="col-md-2">
+            <div class="p-1">
+                <a href="#" class="text-decoration-none" style="color: white; pointer-events: none; cursor: default">
+                    <img src="../img/icons/phone.png" height="20" width="20"></img>
+                    8-800-555-35-35
+                </a>
+            </div>
+        </div>
+        <div class="col-md-1">
+            <div class="p-1"> </div>
+        </div>
+
+    </div>
+</div>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="p-4"></div>
+        </div>
+    </div>
+</div>
+
+
+<nav class="navbar navbar-expand-md navbar-white bg-white front">
+    <div class="container">
+        <a href="#" class="navbar-brand h1" style="font-size: 2.5rem; padding-left:2.4rem; color: #ff8c00;font-weight: bolder">DNS</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+                <li class="nav-item dropdown" style="padding-right: 20px">
+                    <div class="dropdown">
+                        <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false"
+                                style="font-weight: bold; width: 8rem; height: 3rem">
+                            Каталог
+                        </button>
+                        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                            <li><a class="dropdown-item" href="#">Действие</a></li>
+                            <li><a class="dropdown-item" href="#">Другое действие</a></li>
+                            <li><a class="dropdown-item" href="#">Что-то еще здесь</a></li>
+                        </ul>
+                    </div>
+                </li>
+                <li>
+                    <form class="d-flex">
+                        <input class="form-control me-2" type="search" placeholder="Поиск по сайту" aria-label="Search" style="width: 25rem">
+                        <button class="btn btn-outline-success" type="submit">Поиск</button>
+                    </form>
+                </li>
+                <li class="nav-item" style="padding-left: 20px">
+
+                    <a class="nav nav-link" href="#" style="--bs-link-hover-color: #ff8c00; --bs-link-color: black">
+                        <img src="../img/icons/favourite.png" height="20" width="20"></img>
+                        Избранное
+                    </a>
+                </li>
+                <li class="nav-item" style="padding-left: 20px">
+                    <a class="nav nav-link" href="#" style="--bs-link-hover-color: #ff8c00; --bs-link-color: black">
+                        <img src="../img/icons/cart.png" height="20" width="20"></img>
+                        Корзина
+                    </a>
+                </li>
+                <?php
+                if(isset($_SESSION['user']) && !empty($_SESSION['user'])){
+                ?>
+                    <li class="nav-item" style="padding-left: 20px">
+                        Вы вошли как <?php echo $_SESSION['user'].'.'; ?>
+                        <a class="nav nav-link red" href="logout.php" style="--bs-link-hover-color: #ff8c00; --bs-link-color: black; display:inline">
+                            Выйти.
+                        </a>
+                    </li>
+                <?php
+                }else{
+                ?>
+                    <li class="nav-item" style="padding-left: 20px">
+                        Вы не авторизованы.
+                        <a class="nav nav-link" href="login.php" style="--bs-link-hover-color: #ff8c00; --bs-link-color: black; display:inline">
+                            Ввести логин и пароль
+                        </a>
+                        или
+                        <a class="nav nav-link" href="signup.php" style="--bs-link-hover-color: #ff8c00; --bs-link-color: black; display:inline">
+                            зарегестрироваться
+                        </a>
+                    </li>
+                <?php
+                }
+                ?>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+
+<div class="container-fluid" style="margin-top:5rem">
+    <div class="row">
+        <div class="col-md-1">
+            <div class="p-1">
+
+            </div>
+        </div>
+        <div class="col-md-10">
+            <nav style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a class="a-b-o" href="main_page.php">Главная страница</a></li>
+                    <li class="breadcrumb-item"><a class="a-b-o" href="login.php">Авторизация</a></li>
+                </ol>
+            </nav>
+        </div>
+        <div class="col-md-1"       >
+
+        </div>
+    </div>
+</div>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-1">
+            <div class="p-1">
+
+            </div>
+        </div>
+        <div class="col-md-10">
+
+        </div>
+        <div class="col-md-1">
+
+        </div>
+    </div>
+</div>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-1">
+
+        </div>
+        <div class="col-md-10 mx-auto" style="background-color: white;min-height: 20rem;">
+            <div class="col-md-5 mx-auto">
+                <div class="text-center">
+                    <h3><b>Авторизация</b></h3>
+                    <?php
+
+                    if(isset($_SESSION['reg_success'])){
+                        echo '<p class="green"> '.$_SESSION['reg_success'].'</p>';
+                    }
+                    unset($_SESSION['reg_success']);
+
+                    ?>
+                    <?php
+                    if(!isset($_SESSION['user'])){
+                    ?>
+                    <form action="login.php" method="POST">
+                        <input type="hidden" name="action" value="signin">
+                        <div class="form-group">
+                            <label for="email">Email</label>
+                            <input type="email" name="email" class="form-control" placeholder="example@example.com" value="<?php if(isset($_POST['email'])){ echo $_POST['email'];}?>">
+                            <span class="red"><?php if(!empty($arrErr['email'])){echo $arrErr['email'];} ?></span>
+                        </div>
+                        <div class="form-group">
+                            <label for="password">Пароль</label>
+                            <input type="password" name="password" id="password" class="form-control" placeholder="**********">
+                            <span class="red"><?php if(!empty($arrErr['password'])){echo $arrErr['password'];} ?></span>
+                        </div>
+                        <div class="col-md-12 text-center ">
+                            <button type="submit" class=" btn btn-block mybtn btn-primary tx-tfm login-btn" style="margin-top: 1rem">
+                                Войти
+                            </button>
+                        </div>
+                        <div class="form-group">
+                            <p class="text-center">Ещё нет аккаунта? <a href="signup.php">Зарегистрируйтесь</a></p>
+                        </div>
+                    </form>
+                    <?php
+                    }else{
+                        echo '<p class="text-warning"> Вы уже авторизованы как ' . $_SESSION['user'] . '</p>' . '<a class="red" href="logout.php">Выйти.</a>';
+                    }
+                    ?>
+                    <?php
+
+                    if(isset($_SESSION['login_success'])){
+                        echo '<p class="text-warning mt-5">' . $_SESSION['login_success'] . '</p>';
+                    }
+                    unset($_SESSION['login_success']);
+
+                    ?>
+                </div>
+            </div>
+
+
+        </div>
+        <div class="col-md-1">
+
+        </div>
+    </div>
+</div>
+
+
+
+
+</body>
+<footer>
+    <!--Icons by <a target="_blank" href="https://icons8.com">Icons8</a>-->
+</footer>
+</html>
